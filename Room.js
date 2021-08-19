bcbd0b8bd6b20f3f279a8ca904c47fa0af652ba1
sanchayata-jain <sanchayata-jain@users.noreply.github.com>
2021-08-19 18:06:15 +0100

    created functions for each interact object which inherits from base class
    each function will return true or false to a new function in room class called interactWithItem.

--- Room.js
+++ Room.js
@@ -1,34 +1,39 @@
-// class Room {
-//     constructor(name, frontItems, backItems, rightItems, leftItems){
-//         this.name = name;
-//         this.frontRoomItems = frontItems;
-//         this.backRoomItems = backItems;
-//         this.leftRoomItems = rightItems;
-//         this.rightRoomItems = leftItems;
-//         this.interactionItem;
-//         this.interactionItemPosition;
-//     }
+class Room {
+    constructor(name, frontItems, backItems, rightItems, leftItems){
+        this.name = name;
+        this.frontRoomItems = frontItems;
+        this.backRoomItems = backItems;
+        this.leftRoomItems = rightItems;
+        this.rightRoomItems = leftItems;
+        this.interactionItem;
+        this.interactionItemPosition;
+        this.roomComplete = false; 
+    }
 
-//     displayAllItemsDirection(direction) {
-//         // displays all the items in whichever direction user inputs 
+    displayAllItemsDirection(direction) {
+        // displays all the items in whichever direction user inputs
         
-//     }
+    }
 
-//     displayPickupItem() {
-//         // displays all the pickup items in whichever direction user input
+    displayPickupItemDirection(direction) {
+        // displays all the pickup items in whichever direction user input
 
-//     }
+    }
 
-// }
+    interactWithItem(item) {
+        this.roomComplete = interactionItem.interact(item);
+    }
 
-// var frontItemsAttic = [new PickupItem("knife"), new PickupItem("remote")];
-// var backItemsAttic = [];
-// var rightItemsAttic = [];
-// var leftItemsAttic = [new PickupItem("fan"), new PickupItem("taco")];  //list of objects
+}
 
-// var frontItemsBedroom = [];
+var frontItemsAttic = [new PickupItem("knife"), new PickupItem("remote")];
+var backItemsAttic = [];
+var rightItemsAttic = [];
+var leftItemsAttic = [new PickupItem("fan"), new PickupItem("taco")];  //list of objects
 
-// attic = new Room("attic", frontItemsAttic, backItemsAttic, rightItemsAttic, leftItemsAttic);
+var frontItemsBedroom = [];
+
+attic = new Room("attic", frontItemsAttic, backItemsAttic, rightItemsAttic, leftItemsAttic);
 
 
 
