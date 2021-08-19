3c4783ce739b64670f00f77eda307f127d8e5252
sanchayata-jain <sanchayata-jain@users.noreply.github.com>
2021-08-19 16:01:58 +0100

    basic structure to game
    created files and classes which can be changed as needed.

---
+++ Player.js
@@ -0,0 +1,18 @@
+// class Player {
+//     constructor() {
+//         this.inventory = [];
+//     }
+
+//     pickupItem(item) {
+//         this.inventory.push(item);
+//     }
+
+//     displayInventory() {
+//         //iterate through inventory list
+//     }
+// }
+
+// player1 = new Player();
+// player1.pickupItem(new PickupItem("knife"));
+// player1.displayInventory();
+
---
+++ Room.js
@@ -0,0 +1,35 @@
+// class Room {
+//     constructor(name, frontItems, backItems, rightItems, leftItems){
+//         this.name = name;
+//         this.frontRoomItems = frontItems;
+//         this.backRoomItems = backItems;
+//         this.leftRoomItems = rightItems;
+//         this.rightRoomItems = leftItems;
+//         this.interactionItem;
+//         this.interactionItemPosition;
+//     }
+
+//     displayAllItemsDirection(direction) {
+//         // displays all the items in whichever direction user inputs 
+        
+//     }
+
+//     displayPickupItem() {
+//         // displays all the pickup items in whichever direction user input
+
+//     }
+
+// }
+
+// var frontItemsAttic = [new PickupItem("knife"), new PickupItem("remote")];
+// var backItemsAttic = [];
+// var rightItemsAttic = [];
+// var leftItemsAttic = [new PickupItem("fan"), new PickupItem("taco")];  //list of objects
+
+// var frontItemsBedroom = [];
+
+// attic = new Room("attic", frontItemsAttic, backItemsAttic, rightItemsAttic, leftItemsAttic);
+
+
+
+
