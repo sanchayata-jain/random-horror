7782f7d3dd93e32bdcd8f0452cf02bb062afe309
emmxgibbs <emma.gibbs@live.co.uk>
2021-08-20 14:13:51 +0100

    added some code to player.js

--- Player.js
+++ Player.js
@@ -1,18 +1,30 @@
-// class Player {
-//     constructor() {
-//         this.inventory = [];
-//     }
+import PickupItem from '.PickupItem';
+//player needs to be able to pick up items
 
-//     pickupItem(item) {
-//         this.inventory.push(item);
-//     }
+export default class Player {
+    //constructor to give player a name, the ability to create an inventory and the ability to assign a current room
+    constructor(name = '', inventory = new Inventory(), currentRoom = '', startRoom) {
+        //allows player to be given name and assigns the name as the player
+        this.name = name;
+            if (this.name === '') {
+                this.name = 'player';
+            }
+        //creates a new inventory for the player
+        this.inventory = [];
+    }
 
-//     displayInventory() {
-//         //iterate through inventory list
-//     }
-// }
+    pickupItem(item) {
+        this.inventory.push(item);
+    }
 
-// player1 = new Player();
-// player1.pickupItem(new PickupItem("knife"));
-// player1.displayInventory();
+    displayInventory() {
+        //iterate through inventory list
+        for (var item of this.inventory) {
+            item.displayName();
+        }
+    }
+}
 
+player1 = new Player();
+player1.pickupItem(new PickupItem("knife"));
+player1.displayInventory();
